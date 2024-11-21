6e5150c1c7f21b4cad1db60491d1a27ff351dfbf
correo@gmaiul.com <correo@gmaiul.com>
2024-11-21 13:46:41 +0100

    solo en esta ramaa

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ import java.sql.SQLOutput;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HOLA MUNDO");
+        System.out.println("HOLA");
     }
 }
