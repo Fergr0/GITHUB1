1a2a1aecc2e850c6da94bfddd2f9129415421b78
correo@gmaiul.com <correo@gmaiul.com>
2024-11-21 13:55:35 +0100

    bbbbbbbbbb

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,1 @@
-import java.sql.SQLOutput;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("HOLA QUE PASA");
-        System.out.println("HOLAAAAAAAAAAA");
-    }
-}
+aaaaaaaaa
